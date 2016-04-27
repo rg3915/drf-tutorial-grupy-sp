3a460d3b0215a8d4590f0597d8c4fa689e43a27c
rg3915 <rg3915@yahoo.com.br>
2016-04-27 19:55:41 -0300

    urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -8,5 +8,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^api/', include('myproject.core.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
