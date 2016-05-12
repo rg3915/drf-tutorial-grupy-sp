72e817cae9384bdc6fe6f72f53114ba5f56c9429
rg3915 <rg3915@yahoo.com.br>
2016-05-12 17:49:23 -0300

    admin

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,6 @@
 # urls.py
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework import routers
 from myproject.core import views
 
@@ -9,5 +10,6 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api/', include('myproject.core.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
